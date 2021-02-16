785c5eec29b8fb3c58fb22c168e71302d53ccc48
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2021-02-16 18:24:20 -0400

    teste
    alterando css

--- pacote-download/src/components/TaskList/index.js
+++ pacote-download/src/components/TaskList/index.js
@@ -20,27 +20,4 @@ export default function TaskList({data, handleDelete}){
     )
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        margin: 8,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor:'#FFF',
-        borderRadius: 5,
-        padding: 7,
-        elevation: 1.5,
-        shadowColor:'#000',
-        shadowOpacity:0.2,
-        shadowOffset:{
-            width: 1,
-            height: 3,
-        }
-    },
-    task:{
-        color:'#121212',
-        fontSize: 20,
-        paddingLeft: 10,
-        paddingRight: 20,
-    }
-});
+
